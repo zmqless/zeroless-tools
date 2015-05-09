ba85e2418d635a1b2487edd5ef756a3cdaaa9d63
Lucas Lira Gomes <x8lucas8x@gmail.com>
2015-05-08 23:10:16 -0300

    Added tests to the pubsub pattern for both client and server.

---
+++ tests/test_pubsub.py
@@ -0,0 +1,52 @@
+import io
+import sys
+import pytest
+
+import zeroless_tools
+
+from time import sleep
+
+from zeroless_tools import helpers
+from zeroless_tools import server
+from zeroless_tools import client
+from zeroless_tools import SocketExecutor
+
+class TestPubSub:
+    def test_executors(self):
+        server_socket_executor = zeroless_tools.server.zeroserver('7894 pub'.split())
+        client_socket_executor = zeroless_tools.client.zeroclient('7894 sub'.split())
+
+        assert isinstance(server_socket_executor, SocketExecutor.PubExecutor)
+        assert isinstance(client_socket_executor, SocketExecutor.SubExecutor)
+
+    def test_server_to_client(self):
+        msg = ['msg1\n']
+
+        stdin_old = sys.stdin
+        stdout_old = sys.stdout
+
+        try:
+            input = io.StringIO()
+            output = io.StringIO()
+
+            sys.stdin = input
+            sys.stdout = output
+
+            input.writelines(msg)
+            input.seek(0)
+
+            server_socket_executor = zeroless_tools.server.zeroserver('7895 pub'.split())
+            client_socket_executor = zeroless_tools.client.zeroclient('7895 sub'.split())
+
+            sleep(0.1)
+            helpers.run(server_socket_executor)
+            helpers.run(client_socket_executor)
+
+            output.seek(0)
+            received_msgs = output.readlines()
+
+        finally:
+            sys.stdin = stdin_old
+            sys.stdout = stdout_old
+
+        assert msg == received_msgs
