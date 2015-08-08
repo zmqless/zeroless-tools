de3342e9aff1caaab1063d33d3083fd85cf66b23
Lucas Lira Gomes <x8lucas8x@gmail.com>
2015-08-08 12:21:45 -0300

    Updated patch version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def readme():
         return f.read()
 
 setup(name='zeroless-tools',
-      version='0.2.1',
+      version='0.2.2',
       description='CLI Tools for ZeroMQ™',
       long_description=readme(),
       packages=['zeroless_tools'],
